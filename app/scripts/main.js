916fc0bbf7a2d63b4fd278eafa334e4d7c1ecf65
Whitney Williams <wweliz@gmail.com>
2014-10-18 11:05:50 -0400

    basic backbone set up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,40 @@
-console.log('\'Allo \'Allo!');
+/* global _, Backbone */
+'use strict';
+
+// LOG IN VIEW //////////////////////////////////
+var LogInView = Backbone.View.extend({
+	className : 'login',
+	loginTemplate: _.template($('.login-template').text()),
+
+	initialize: function(){
+		//appends login-view div with contents of the login-template
+		$('.login-view').append(this.el);
+	},
+
+	render: function(){
+		this.$el.html(this.loginTemplate);
+		return this;
+	}
+
+});
+
+// THE APP ROUTER ///////////////////////////////
+var AppRouter = Backbone.Router.extend({
+	routes: {
+		//URL to match	//function called when the hash matches
+		''				: 'renderLogIn',				//	url/#
+	},
+
+	initialize: function(){
+
+	},
+
+  renderLogIn: function(){
+  	new LogInView();
+  }
+
+});
+
+// INSTANTIATING THE ROUTER /////////////////////
+new AppRouter();
+Backbone.history.start();
