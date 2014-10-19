91ca91b457bb2840e2e817bcd852ef088b020877
Whitney Williams <wweliz@gmail.com>
2014-10-18 20:30:30 -0400

    progress towards user sign up and login; checking current user status before displaying each view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,18 +6,92 @@ var LogInView = Parse.View.extend({
 	className : 'login',
 	loginTemplate: _.template($('.login-template').text()),
 
+	events: {
+		'click .login-btn'	: 'userLogIn',
+	},
+
 	initialize: function(){
 		//appends login-view div with contents of the login-template
 		$('.login-view').html(this.el);
-		this.render();
 	},
 
 	render: function(){
 		this.$el.html(this.loginTemplate);
 		return this;
+	},
+
+	userLogIn: function(){
+		//naming the value of the input fields
+		var usernameVal = $('.username-input').val();
+		var passwordVal = $('.password-input').val();
+		
+		//calls Parse's login function
+		Parse.User.logIn(usernameVal, passwordVal, {
+		  success: function(user) {
+				console.log('Username', user.get('username'), 'is logged in.');
+				router.navigate('userview', {trigger: true});
+			},
+
+		  error: function(user, error) {
+		  	//alert('Error: Log in failed.');
+		  	console.log('User not logged in.' + error.code + " " + error.message);
+				//user stays on the login page
+		  }
+		});
 	}
 });
-console.log('render function ran');
+
+// SIGN UP VIEW ///////////////////////////////////////////////////////////
+var SignUpView = Parse.View.extend({
+	
+	signupTemplate: _.template($('.signup-template').text()),
+
+	events: {
+		'click .signup-btn' : 'userSignUp'
+	},
+
+	initialize: function(){
+		//appends signup-view div with contents of the signup-template
+		$('.signup-view').html(this.el);
+	},
+
+	render: function(){
+		this.$el.html(this.signupTemplate);
+		return this;
+	},
+
+	userSignUp: function(){
+		//naming the value of the input fields
+		var usernameVal = $('.username-input').val();
+		var emailVal = $('.email-input').val();
+		var passwordVal = $('.password-input').val();
+		var hometownVal = $('.hometown-input').val();
+
+		//creates a user instance; adds that instance to the users collection
+		//sets the properties of that user to be the value of the input fields
+		var user = new Parse.User();
+		user.set({
+			'username':		usernameVal,
+			'email':			emailVal,
+			'password':		passwordVal,
+			'hometown': 	hometownVal
+		});
+
+		//saves added input to the server; when done...
+		user.signUp(null, {
+			success: function(user) {
+				console.log('Username', user.get('username'), 'is logged in.');
+    		router.navigate('userview', {trigger: true});
+  		},
+  		error: function(user, error) {
+    		//alert('Error: Sign up failed.');
+				console.log('User not logged in.' + error.code + ' ' + error.message);
+				//user stays on the signup page
+  		}
+		});
+	}
+
+});
 
 // ABOUT ME VIEW ////////////////////////////////
 var AboutMeView = Parse.View.extend({
@@ -27,7 +101,6 @@ var AboutMeView = Parse.View.extend({
 	initialize: function(){
 		//appends about-me-view div with contents of the about-me-template
 		$('.about-me-view').html(this.el);
-		this.render();
 	},
 
 	render: function(){
@@ -46,7 +119,6 @@ var SetLanguagesView = Parse.View.extend({
 	initialize: function(){
 		//appends set-lang-view div with contents of the set-lang-template
 		$('.set-lang-view').html(this.el);
-		this.render();
 	},
 
 	render: function(){
@@ -64,7 +136,6 @@ var ToMeetView = Parse.View.extend({
 	initialize: function(){
 		//appends to-meet-view div with contents of the login-template
 		$('.to-meet-view').html(this.el);
-		this.render();
 	},
 
 	render: function(){
@@ -82,7 +153,6 @@ var MessengerView = Parse.View.extend({
 	initialize: function(){
 		//appends messenger-view div with contents of the messenger-template
 		$('.messenger-view').html(this.el);
-		this.render();
 	},
 
 	render: function(){
@@ -108,26 +178,63 @@ var AppRouter = Parse.Router.extend({
 
 	initialize: function(){
 		this.currentView = null;
+
+		// if there is a currently logged in user...
+		if ( Parse.User.current() ) {
+			//router redirects to the AboutMeView
+			this.navigate('aboutme', {trigger: true});
+
+		// if there is NOT a currently logged in user...
+		} else {
+			//router redirects to the LogInView
+			this.navigate('', {trigger: true});
+	},
+
+	// REDIRECT TO LOGIN IF NO CURRENT USER
+	redirectToLogin: function(){
+		this.navigate('', {trigger: true});
 	},
 
   renderLogIn: function(){
   	this.swap( new LogInView() );
+  	$('.menubar').hide();
   },
 
   renderAboutMe: function(){
-  	this.swap( new AboutMeView() );
+	if ( Parse.User.current() === null ){
+			this.redirectToLogin();
+		} else {
+			//instantiate the AboutMeView with the current user as the model
+			this.swap( new AboutMeView({model: Parse.User.current().attributes}) );
+			$('.menubar').show();
+		}
   },
 
   renderSetLanguages: function(){
-  	this.swap( new SetLanguagesView() );
+  if ( Parse.User.current() === null ){
+			this.redirectToLogin();
+		} else {
+			this.swap( new SetLanguagesView() );
+			$('.menubar').show();
+		}
   },
 
   renderToMeet: function(){
-  	this.swap( new ToMeetView() );
+  if ( Parse.User.current() === null ){
+			this.redirectToLogin();
+		} else {
+			this.swap( new ToMeetView() );
+			$('.menubar').show();
+		}
   },
 
   renderMessenger: function(){
-  	this.swap( new MessengerView() );
+  if ( Parse.User.current() === null ){
+			this.redirectToLogin();
+		} else {
+			this.swap( new MessengerView() );
+			$('.menubar').show();
+		}
   },
 
   swap: function(view){
